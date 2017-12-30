f30d550d7b47f7c5c83eaa59d4c313a948225774
Mohsan <mohsannaeem03@hotmail.com>
2017-12-30 16:12:01 +0500

    Uploaded All Files

---
+++ script.js
@@ -0,0 +1,34 @@
+// var name = 'mohsan khan';
+// alert(name);
+// console.log(name);
+ 
+// var name = 'atif shaikh';
+// alert(name);
+// console.log(name);
+
+// var number = 10;
+// console.log(number + 10);
+
+// var city = 'lahore';
+// var country = 'pakistan';
+
+// console.log(city + ' - ' + country);
+ 
+// alert(city + ' - ' + country);
+// alert(number + 10);
+// alert(number);
+
+// var num = 4;
+
+// num++;
+// console.log(num);
+// alert(num);
+
+
+
+
+
+
+
+var message = 'Would you like to visit this website?'
+alert (message);
